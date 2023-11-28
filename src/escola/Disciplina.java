d1deada10fee900e558837cb9f62f9488213f3a3
Iuri Néviton <iurineviton@gmail.com>
2023-11-27 21:44:09 -0300

    primeiras classes

--- src/escola/Disciplina.java
+++ src/escola/Disciplina.java
@@ -4,13 +4,6 @@ public class Disciplina {
     private String nome;
     private Professor professor;
 
-    public Disciplina criarDisciplina(String nome, Professor professor) {
-        Disciplina disciplina = new Disciplina();
-        disciplina.setNome(nome);
-        disciplina.setProfessor(professor);
-        return disciplina;
-    }
-
     public String getNome() {
         return nome;
     }
